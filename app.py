c247ee363e8a39218dee6d2eac88cf74599263c7
BoberMod <BoberMod@yandex.ru>
2019-06-27 20:08:12 +0300

    Configure the database and imports in the main file

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+import models
 
 
 @app.route('/')
